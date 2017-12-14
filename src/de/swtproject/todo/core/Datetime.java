ade54da221a1eb4ba38e13c028bda790d84b173d
Horft <horft@live.com>
2017-12-14 14:34:08 +0100

    Datetime

---
+++ src/de/swtproject/todo/core/Datetime.java
@@ -0,0 +1,26 @@
+package de.swtproject.todo.core;
+
+public class Datetime {
+
+	private Date dueDate;
+
+	public Date() {
+
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public String getDate() {
+		return new SimpleDateFormat("hh.MM.yyyy").
+	}
+
+	public String getTime() {
+
+	}
+}
